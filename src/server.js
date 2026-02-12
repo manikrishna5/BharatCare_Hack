9f01e36299a3056fe8e50dfc352106777d413f71
Mani Krishna Karri <manikrishnakarri@gmail.com>
2026-02-12 11:04:57 +0530

    Node modules

---
+++ src/server.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("BharathCare API running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+process.on("uncaughtException", (err) => {
+	console.error("Uncaught Exception:", err);
+	process.exit(1);
+});
+
+process.on("unhandledRejection", (reason) => {
+	console.error("Unhandled Rejection:", reason);
+	process.exit(1);
+});
+
+const startServer = async () => {
+	try {
+		await connectDB();
+		app.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+	} catch (err) {
+		console.error("Failed to start server:", err);
+		process.exit(1);
+	}
+};
+
+startServer();
